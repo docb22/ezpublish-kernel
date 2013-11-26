dbe81c2f64076fe15d02a5a10f2b423a712e33af
Christian Bacher <cba@ez.no>
2013-11-26 13:02:32 +0100

    SPI: refactored user/group/role
    - added create/update structs for user/role/policy
    - added role package with role handler
    - refactored assignments
    - moved group handelr into user handler

--- eZ/Publish/SPI/Persistence/User/Role/CreateStruct.php
+++ eZ/Publish/SPI/Persistence/User/Role/CreateStruct.php
@@ -1,27 +1,23 @@
 <?php
 /**
- * File containing the eZ\Publish\SPI\Persistence\User\Role class
+ * File containing the eZ\Publish\SPI\Persistence\User\Role\CreateStruct class.
  *
  * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
  * @version //autogentag//
  */
 
-namespace eZ\Publish\SPI\Persistence\User;
+
+namespace eZ\Publish\SPI\Persistence\User\Role;
 
 use eZ\Publish\SPI\Persistence\MultiLanguageValueBase;
-use eZ\Publish\SPI\Persistence\ValueObject;
 
 /**
+ * this class is used to create a role
+ *
+ * @package eZ\Publish\SPI\Persistence\User\Role
  */
-class Role extends MultiLanguageValueBase
-{
-    /**
-     * ID of the user rule
-     *
-     * @var mixed
-     */
-    public $id;
+class CreateStruct extends MultiLanguageValueBase {
 
     /**
      * Policies associated with the role
@@ -43,5 +39,4 @@ class Role extends MultiLanguageValueBase
      * @var mixed[] In LE implementation, id's are contentId's
      */
     public $userIds = array();
-
 }
